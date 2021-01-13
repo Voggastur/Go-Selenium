e3157243215479dfb76f970ae9884a162c504ac3
Johan Kjellin <55715590+Voggastur@users.noreply.github.com>
2021-01-13 14:17:59 +0100

    Update routes_test.go

--- pensionera/routes_test.go
+++ pensionera/routes_test.go
@@ -8,12 +8,13 @@ import (
   "testing"
 )
 
+
 func TestCreateBook(t *testing.T) {
 
   url := "http://localhost:8080/api/v1/book"
   method := "POST"
 
-  payload := strings.NewReader("BookID=3&Title=Vulcan%20Philosophy&Authors=Huber%20Ugbert&ISBN=3&ISBN13=1000000000003&Ratings=25&Reviews=15&NumPages=999&LanguageCode=eng&AverageRating=2.5")
+  payload := strings.NewReader("BookID=2&Title=Johans%20Polar%20Expedition&Authors=Albert%20Einstein&ISBN=2&ISBN13=1000000000002&Ratings=25&Reviews=15&NumPages=999&LanguageCode=eng&AverageRating=1.6")
 
   client := &http.Client {
   }
@@ -40,9 +41,37 @@ func TestCreateBook(t *testing.T) {
   fmt.Println(string(body))
 }
 
+
 func TestSearchByISBN(t *testing.T) {
-  url := "http://localhost:8080/api/v1/book/isbn/3"
+
+  url := "http://localhost:8080/api/v1/book/isbn/2"
   method := "GET"
+  client := &http.Client {
+  }
+  req, err := http.NewRequest(method, url, nil)
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+  res, err := client.Do(req)
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+  defer res.Body.Close()
+  body, err := ioutil.ReadAll(res.Body)
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+  fmt.Println(string(body))
+}
+
+
+func TestDeleteBook(t *testing.T) {
+
+  url := "http://localhost:8080/api/v1/book/isbn/2"
+  method := "DELETE"
   client := &http.Client {
   }
   req, err := http.NewRequest(method, url, nil)
