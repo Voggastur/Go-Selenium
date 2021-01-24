89bc054d608dcf083d2cd1e0a57be1271d154254
Johan Kjellin <55715590+Voggastur@users.noreply.github.com>
2021-01-25 00:09:04 +0100

    Update routes_test.go

--- pensionera/routes_test.go
+++ pensionera/routes_test.go
@@ -4,9 +4,7 @@ import (
     "testing"
     "net/http"
     "io/ioutil"
-    "fmt"
     "strings"
-    "log"
 )
 
 
@@ -20,32 +18,24 @@ func TestSearchBookByISBN(t *testing.T) {
 
     req, err := http.NewRequest(method, url+isbn, nil)
     if err != nil {
-        fmt.Println("HTTP call failed", err)
-        return
+	    t.Fatalf(err.Error())
     }
     res, err := client.Do(req)
     if err != nil {
-        fmt.Println("HTTP call failed:", err)
-	return
+	    t.Fatalf(err.Error())
     }
-    if res.StatusCode >= 200 && res.StatusCode <= 299 {
-        fmt.Println("Success, HTTP Status is in the 2xx range: Got ", res.StatusCode)
-    } else {
-        fmt.Println("HTTP Response not in the 2xx range: Got ", res.StatusCode)
+    if res.StatusCode <= 200 && res.StatusCode >= 299 {
+	    t.Errorf("FAIL, HTTP Status is outside of the 2xx range: Got %v", res.StatusCode)
     }
     defer res.Body.Close()
     body, err := ioutil.ReadAll(res.Body)
     if err != nil {
-	fmt.Println(err)
-	return
+	    t.Fatalf(err.Error())
     }
     expected := `{"book_id":"43509","title":"Harry Potter and the Goblet of Fire (Harry Potter  #4)","authors":"J.K. Rowling","average_rating":4.55,"isbn":"074754624X","isbn_13":"9780747546245","language_code":"eng","num_pages":636,"ratings":18097,"reviews":860}`
     if string(body) != expected {
-        fmt.Println("Fail: The found book does not match expected book")
-    } else {
-        fmt.Println("Success: The found book matches the expected Harry Potter book")
+	    t.Errorf("Fail: The found book does not match expected book")
     }
-    fmt.Println(string(body))
 }
 
 
@@ -53,33 +43,25 @@ func TestSearchByAuthor(t *testing.T) {
 
     req, err := http.NewRequest("GET", "http://localhost:8080/api/v1/books/authors/galileo%20galilei", nil)
     if err != nil {
-        log.Fatal("HTTP Request Failed: Got ", err)
+	    t.Fatalf(err.Error())
     }
-
     client := &http.Client {}
     res, err := client.Do(req)
     if err != nil {
-        fmt.Println("HTTP call failed: Got ", err)
-        return
+	    t.Fatalf(err.Error())
     }
-    if res.StatusCode >= 200 && res.StatusCode <= 299 {
-        fmt.Println("Success: HTTP Status is in the 2xx range: Got ", res.StatusCode)
-    } else {
-        fmt.Println("Fail: HTTP Response not what expected, Got: ", res.StatusCode)
+    if res.StatusCode <= 200 && res.StatusCode >= 299 {
+	    t.Errorf("Fail: HTTP Response outside 2xx range, Got: %v", res.StatusCode)
     }
     defer res.Body.Close()
     body, err := ioutil.ReadAll(res.Body)
     if err != nil {
-        fmt.Println(err)
-        return
+	    t.Fatalf(err.Error())
     }
     expected := `[{"book_id":"32610","title":"On the Shoulders of Giants: The Great Works of Physics and Astronomy","authors":"Stephen Hawking-Isaac Newton-Nicolaus Copernicus-Albert Einstein-Johannes Kepler-Galileo Galilei","average_rating":4.2,"isbn":"0762427329","isbn_13":"9780762427321","language_code":"eng","num_pages":256,"ratings":4014,"reviews":34}]`
     if string(body) != expected {
-        fmt.Println("Fail: Does not match expected book")
-    } else {
-        fmt.Println("Success: Found expected book by Galileo Galilei")
+	    t.Errorf("Fail: Does not match expected book")
     }
-    fmt.Println(string(body))
 }
 
 
@@ -87,39 +69,28 @@ func TestCreateBook(t *testing.T) {
 
   url := "http://localhost:8080/api/v1/book"
   method := "POST"
-
-  payload := strings.NewReader("BookID=2&Title=Johans%20Marmelade%20Tips&Authors=Jules%20Verne&ISBN=2&ISBN13=1000000000002&Ratings=25&Reviews=15&NumPages=999&LanguageCode=eng&AverageRating=1.6")
-
+  payload := strings.NewReader("BookID=2&Title=My%20Marmelade%20Tips&Authors=Jules%20Verne&ISBN=2&ISBN13=1000000000002&Ratings=25&Reviews=15&NumPages=999&LanguageCode=eng&AverageRating=1.6")
   client := &http.Client {}
-  req, err := http.NewRequest(method, url, payload)
 
+  req, err := http.NewRequest(method, url, payload)
   if err != nil {
-    fmt.Println("Fail: HTTP Call failed: ", err)
-    return
+	  t.Fatalf(err.Error())
   }
   req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-
   res, err := client.Do(req)
   if err != nil {
-    fmt.Println(err)
-    return
+	  t.Fatalf(err.Error())
   }
-
   if res.StatusCode != 201 {
-    fmt.Println("HTTP Status not what expected: Got ", res.StatusCode)
-  } else {
-    fmt.Println("Got expected 201 HTTP Status: Got ", res.StatusCode)
+	  t.Errorf("HTTP Status not what expected: got %v", res.StatusCode)
   }
   defer res.Body.Close()
   body, err := ioutil.ReadAll(res.Body)
   if err != nil {
-    fmt.Println(err)
-    return
+	  t.Fatalf(err.Error())
   }
   if string(body) != `{"success": "created"}` {
-    fmt.Println("Success created not found, instead got: ", string(body))
-  } else {
-    fmt.Println("Test passed - Got expected success message ", string(body))
+	  t.Errorf("FAIL: Success created not found, got: %v", string(body))
   }
 }
 
@@ -133,26 +104,24 @@ func TestReadRecentCreatedBook(t *testing.T) {
 
     req, err := http.NewRequest(method, url+isbn, nil)
     if err != nil {
-        fmt.Println(err)
-        return
+	    t.Fatalf(err.Error())
     }
     res, err := client.Do(req)
     if err != nil {
-        fmt.Println("HTTP call failed: ", err)
-        return
+	    t.Fatalf(err.Error())
     }
-    if res.StatusCode >= 200 && res.StatusCode <= 299 {
-        fmt.Println("Success: HTTP Status is in the 2xx range ", res.StatusCode)
-    } else {
-        fmt.Println("Fail: HTTP Response not what expected: ", res.StatusCode)
+    if res.StatusCode <= 200 && res.StatusCode >= 299 {
+	    t.Errorf("Fail: HTTP Status is not in the 2xx range, got this: %v ", res.StatusCode)
     }
     defer res.Body.Close()
     body, err := ioutil.ReadAll(res.Body)
     if err != nil {
-        fmt.Println(err)
-        return
+	    t.Fatalf(err.Error())
     }
-    fmt.Println(string(body))
+    expected := `{"book_id":"2","title":"My Marmelade Tips","authors":"Jules Verne","average_rating":1.6,"isbn":"2","isbn_13":"1000000000002","language_code":"eng","num_pages":999,"ratings":25,"reviews":15}`
+    if string(body) != expected {
+	    t.Errorf("Fail: Does not match expected book")
+    }
 }
 
 
@@ -166,30 +135,23 @@ func TestDeleteCreatedBook(t *testing.T) {
 
     req, err := http.NewRequest(method, url+isbn, nil)
     if err != nil {
-        fmt.Println("HTTP call failed", err)
-        return
+	    t.Fatalf(err.Error())
     }
     res, err := client.Do(req)
     if err != nil {
-        fmt.Println("HTTP call failed:", err)
-        return
+	    t.Fatalf(err.Error())
     }
-    if res.StatusCode >= 200 && res.StatusCode <= 299 {
-        fmt.Println("Success: HTTP Status is in the 2xx range: Got ", res.StatusCode)
-    } else {
-        fmt.Println("Fail: HTTP Response not in the 2xx range: Got ", res.StatusCode)
+    if res.StatusCode <= 200 && res.StatusCode >= 299 {
+	    t.Errorf("Fail: HTTP Response not in the 2xx range: Got %v", res.StatusCode)
     }
     defer res.Body.Close()
     body, err := ioutil.ReadAll(res.Body)
     if err != nil {
-        fmt.Println(err)
-        return
+	    t.Fatalf(err.Error())
     }
     expected := `{"success": "deleted"}`
     if string(body) != expected {
-        fmt.Println("Fail: Expected delete but instead got:", string(body))
-    } else {
-        fmt.Println("Success: Got correct delete message: ", string(body))
+	    t.Errorf("FAIL: Expected delete message but instead got: %v", string(body))
     }
 }
 
@@ -203,30 +165,22 @@ func TestSearchDeletedBookByISBN(t *testing.T) {
 
     req, err := http.NewRequest(method, url+isbn, nil)
     if err != nil {
-        fmt.Println("HTTP call failed", err)
-        return
+	    t.Fatalf(err.Error())
     }
     res, err := client.Do(req)
     if err != nil {
-        fmt.Println("HTTP call failed:", err)
-        return
+	    t.Fatalf(err.Error())
     }
-    if res.StatusCode >= 400 && res.StatusCode <= 499 {
-        fmt.Println("PASS: HTTP Status is in the 4xx range ", res.StatusCode)
-    } else {
-        fmt.Println("FAIL: HTTP Status outside the 4xx range: ", res.StatusCode)
+    if res.StatusCode <= 400 && res.StatusCode >= 499 {
+	    t.Errorf("FAIL: HTTP Status is outside of 4xx range, got: %v", res.StatusCode)
     }
     defer res.Body.Close()
     body, err := ioutil.ReadAll(res.Body)
     if err != nil {
-        fmt.Println("Error reading response body")
-        return
+	    t.Fatalf(err.Error())
     }
-
     if string(body) != `{"error": "not found"}` {
-	fmt.Println("Fail: Expected book to be deleted, instead got:", string(body))
-    } else {
-        fmt.Println("Success: Book is correctly missing after previous deletion, got this:", string(body))
+	    t.Errorf("Fail: Expected book to be missing, got: %v", string(body))
     }
 }
 
@@ -235,62 +189,45 @@ func TestCreateBookWithInvalidParameters(t *testing.T) {
 
   url := "http://localhost:8080/api/v1/book"
   method := "POST"
-
-  // This payload has value hello for the BookID field, so should return error message
+  // This payload has value hello for the BookID integer field, so should return error message
   payload := strings.NewReader("BookID=hello&Title=Aliens%20from%20Saturn&Authors=Donald%20Duck&ISBN=1&NumPages=12&LanguageCode=eng")
-
   client := &http.Client {}
-  req, err := http.NewRequest(method, url, payload)
 
+  req, err := http.NewRequest(method, url, payload)
   if err != nil {
-    fmt.Println("Fail: HTTP Call failed: ", err)
-    return
+	  t.Fatalf(err.Error())
   }
   req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-
   res, err := client.Do(req)
   if err != nil {
-    fmt.Println("HTTP Response failed, got:", err)
-    return
+	  t.Fatalf(err.Error())
   }
-
-  if res.StatusCode >= 400 && res.StatusCode <= 499 {
-        fmt.Println("Success: HTTP Status in the 4xx range ", res.StatusCode)
-    } else {
-        fmt.Println("Fail: HTTP Status outside the 4xx range: ", res.StatusCode)
-    }
-
+  if res.StatusCode <= 400 && res.StatusCode >= 499 {
+	  t.Errorf("FAIL: HTTP Status is outside of 4xx range, got this %v", res.StatusCode)
+  }
   defer res.Body.Close()
   body, err := ioutil.ReadAll(res.Body)
   if err != nil {
-    fmt.Println(err)
-    return
+	  t.Fatalf(err.Error())
   }
-
   if string(body) != `{"success": "created"}` {
-    fmt.Println("Success: Book can not be created, got this:", string(body))
+	  return
   } else {
-    fmt.Println("Fail: success created regardless of invalid field(s), message received:", string(body))
-
-    // Request a delete of the irregular book in the else block
-
-    url := "http://localhost:8080/api/v1/book/isbn/"
-    isbn := "1"
-    method := "DELETE"
-    client := &http.Client {}
-
-    req, err := http.NewRequest(method, url+isbn, nil)
-    if err != nil {
-        fmt.Println("HTTP call failed", err)
-        return
-    }
-    res, err := client.Do(req)
-    if err != nil {
-        fmt.Println("HTTP call failed:", err)
-        return
-    }
-    res.Body.Close()
-    fmt.Println("The irregularly created book has been deleted by its ISBN")
+          // Request a delete of the irregular book in the else block
+	  url := "http://localhost:8080/api/v1/book/isbn/"
+	  isbn := "1"
+	  method := "DELETE"
+	  client := &http.Client {}
+	  req, err := http.NewRequest(method, url+isbn, nil)
+	  if err != nil {
+		  t.Fatalf(err.Error())
+	  }
+	  res, err := client.Do(req)
+	  if err != nil {
+		  t.Fatalf(err.Error())
+	  }
+	  res.Body.Close()
+	  t.Errorf("Book was created regardless of string in integer field, the book has been automatically deleted by its ISBN")
   }
 }
 
