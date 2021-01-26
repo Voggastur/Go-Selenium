cb70497f1da1d375a6f18ab9d1d496c0da717c7f
Voggastur <v3x0r_x014@hotmail.com>
2021-01-26 00:32:12 +0000

    log.Println to final condition

--- pensionera/selenium.go
+++ pensionera/selenium.go
@@ -8,38 +8,35 @@ import (
 )
 
 
-// If you want to actually run this example:
-//
-//   1. Ensure the file paths at the top of the function are correct.
-//   2. Remove the word "Example" from the comment at the bottom of the
-//      function.
-//   3. Run:
-//      go test -test.run=Example$ github.com/tebeka/selenium
 func main() {
-	// Start a Selenium WebDriver server instance (if one is not already
-	// running).
+
 	const (
-		// These paths will be different on your system.
-		seleniumPath    = "vendor/selenium-server-standalone-3.4.jar"
-		geckoDriverPath = "vendor/geckodriver-v0.18.0-linux64"
+		seleniumPath    = "vendor/selenium-server-standalone-3.141.59.jar"
+		geckoDriverPath = "vendor/geckodriver-v0.28.0-win64"
 		port            = 8080
 	)
+
 	opts := []selenium.ServiceOption{
 		selenium.StartFrameBuffer(),           // Start an X frame buffer for the browser to run in.
 		selenium.GeckoDriver(geckoDriverPath), // Specify the path to GeckoDriver in order to use Firefox.
 		selenium.Output(os.Stderr),            // Output debug information to STDERR.
 	}
+
 	selenium.SetDebug(true)
 	service, err := selenium.NewSeleniumService(seleniumPath, port, opts...)
 	if err != nil {
 		log.Fatalf(err)
 	}
+
 	defer service.Stop()
+
 	caps := selenium.Capabilities{"browserName": "firefox"}
 	wd, err := selenium.NewRemote(caps, fmt.Sprintf("http://localhost:%d/wd/hub", port))
+
 	if err != nil {
 		log.Fatalf(err)
 	}
+
 	wd.get("https://pensionera.se/bli-medlem")
 	title, err := wd.getTitle()
 	if err != nil {
