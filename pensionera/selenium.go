cbf3d62a39c3cfac1c1715810d04a08cf280db57
Johan Kjellin <55715590+Voggastur@users.noreply.github.com>
2021-01-26 19:12:06 +0100

    changed last print to f string format, fixed erroneous wd.title to wd.Title

--- pensionera/selenium.go
+++ pensionera/selenium.go
@@ -1,52 +1,52 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"log"
-	"github.com/tebeka/selenium"
+        "fmt"
+        "os"
+        "log"
+        "github.com/tebeka/selenium"
 )
 
 
 func main() {
 
-	const (
-		seleniumPath    = "vendor/selenium-server-standalone-3.141.59.jar"
-		geckoDriverPath = "vendor/geckodriver-v0.28.0-win64"
-		port            = 8080
-	)
+        const (
+                seleniumPath    = "selenium-server-standalone-3.141.59.jar"
+                chromeDriverPath = "chromedriver"
+                port            = 4444
+        )
 
-	opts := []selenium.ServiceOption{
-		selenium.StartFrameBuffer(),           // Start an X frame buffer for the browser to run in.
-		selenium.GeckoDriver(geckoDriverPath), // Specify the path to GeckoDriver in order to use Firefox.
-		selenium.Output(os.Stderr),            // Output debug information to STDERR.
-	}
+        opts := []selenium.ServiceOption{
+                selenium.StartFrameBuffer(),           // Start an X frame buffer for the browser to run in.
+                selenium.ChromeDriver(chromeDriverPath), // Specify the path to ChromeDriver in order to use Chrome.
+                selenium.Output(os.Stderr),            // Output debug information to STDERR.
+        }
 
-	selenium.SetDebug(true)
-	service, err := selenium.NewSeleniumService(seleniumPath, port, opts...)
-	if err != nil {
-		log.Fatalf(err.Error())
-	}
+        selenium.SetDebug(true)
+        service, err := selenium.NewSeleniumService(seleniumPath, port, opts...)
+        if err != nil {
+                log.Fatalf(err.Error())
+        }
 
-	defer service.Stop()
+        defer service.Stop()
 
-	caps := selenium.Capabilities{"browserName": "firefox"}
-	wd, err := selenium.NewRemote(caps, fmt.Sprintf("http://localhost:%d/wd/hub", port))
+        caps := selenium.Capabilities{"browserName": "Chrome"}
+        wd, err := selenium.NewRemote(caps, fmt.Sprintf("http://localhost:%d/wd/hub", port))
 
-	if err != nil {
-		log.Fatalf(err.Error())
-	}
+        if err != nil {
+                log.Fatalf(err.Error())
+        }
 
-	wd.Get("https://pensionera.se/bli-medlem")
-	title, err := wd.getTitle()
-	if err != nil {
-		log.Fatalf(err.Error())
-	}
-	expected := string("Bli medlem | Pensionera")
+        wd.Get("https://pensionera.se/bli-medlem")
+        title, err := wd.Title()
+        if err != nil {
+                log.Fatalf(err.Error())
+        }
+        expected := string("Bli medlem | Pensionera")
 
-	if string(title) != expected {
-		log.Println("Fail: Unexpected title found, got: %v, expected: %v", title, expected)
-	}
+        if string(title) != expected {
+                log.Printf("Fail: Unexpected title found, got: %v, expected: %v", title, expected)
+        }
 
-	wd.Quit()
+        wd.Quit()
 }
