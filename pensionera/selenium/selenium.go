11cf3143f5a46c219a3e2db8147253029552ee81
Johan Kjellin <55715590+Voggastur@users.noreply.github.com>
2021-02-19 15:53:52 +0100

    Update selenium.go

--- pensionera/selenium/selenium.go
+++ pensionera/selenium/selenium.go
@@ -2,24 +2,24 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"log"
+	"os"
+	"time"
+
 	"github.com/tebeka/selenium"
 )
 
-
-func main() {
+func login() {
 
 	const (
-		seleniumPath    = "selenium-server-standalone-3.141.59.jar"
+		seleniumPath     = "selenium-server-standalone-3.141.0.jar"
 		chromeDriverPath = "chromedriver"
-		port            = 4444
+		port             = 4444
 	)
 
 	opts := []selenium.ServiceOption{
-		// selenium.StartFrameBuffer(),           // Start an X frame buffer for the browser to run in.
-		selenium.ChromeDriver(chromeDriverPath), // Specify the path to ChromeDriver in order to use Chrome.
-		selenium.Output(os.Stderr),            // Output debug information to STDERR.
+		selenium.ChromeDriver(chromeDriverPath),
+		selenium.Output(os.Stderr),
 	}
 
 	selenium.SetDebug(true)
@@ -32,20 +32,106 @@ func main() {
 
 	caps := selenium.Capabilities{"browserName": "chrome"}
 	wd, err := selenium.NewRemote(caps, fmt.Sprintf("http://localhost:%d/wd/hub", port))
-
 	if err != nil {
 		log.Fatalf(err.Error())
 	}
-	wd.Get("https://pensionera.se/bli-medlem")
+	wd.Refresh()
+	if err := wd.Get("http://localhost:8080/bli-medlem"); err != nil {
+		log.Fatalf(err.Error())
+	}
+	url, err := wd.CurrentURL()
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	if url != ("http://localhost:8080/bli-medlem") {
+		wd.Get("https://pensionera.se/bli-medlem")
+		if err != nil {
+			log.Fatalf(err.Error())
+		}
+	}
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	windowID, err := wd.CurrentWindowHandle()
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	err = wd.MaximizeWindow(windowID)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
 	title, err := wd.Title()
 	if err != nil {
 		log.Fatalf(err.Error())
 	}
 	expected := string("Bli medlem | Pensionera")
 	if string(title) != expected {
-		log.Printf("FAIL: Unexpected title found, got: %v, expected: %v", title, expected)
+		log.Printf("FAIL: Unexpected Title, Got: %v, Expected: %v", title, expected)
 	} else {
 		log.Print("SUCCESS: Title matches expected")
 	}
-	wd.Quit()
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	//personnummer
+	input1, err := wd.FindElement(selenium.ByID, "seed")
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	//email
+	input2, err := wd.FindElement(selenium.ByID, "email")
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	//phonenumber
+	input3, err := wd.FindElement(selenium.ByID, "phone-mobile")
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	//salary
+	input4, err := wd.FindElement(selenium.ByID, "salary")
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	err = input1.SendKeys(`
+	1234
+	`)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	time.Sleep(time.Millisecond * 300)
+	err = input2.SendKeys(`
+	a@hotmail.com
+	`)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	err = input3.SendKeys(`
+	08-00000000
+	`)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	err = input4.SendKeys(`
+	1000000
+	`)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	button, err := wd.FindElement(selenium.ByCSSSelector, "button.btn.btn-sign")
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	if err := button.Submit(); err != nil {
+		log.Fatalf(err.Error())
+	}
+	fmt.Print("Test run finished, Timeout 3 seconds")
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	wd.Close()
+}
+
+func main() {
+	login()
 }
